3bfb165e125329fb663010e5194d63b16ad806c2
Rahul Datta <rahuldatta12@hotmail.com>
2025-02-04 23:29:20 -0500

    converted work flows to functions

--- analyze.py
+++ analyze.py
@@ -1,10 +1,18 @@
-print("\nWelcome to the text analysis tool, I will mine and analyze a body of text from a file you provide")
+# welcome user
+def welcomeuser():
+    print("\nWelcome to the text analysis tool, I will mine and analyze a body of text from a file you provide")
 
 # Get user name 
+def getUsername():
+    usernNamefromInput = input("\nTo begin, please enter your username:\n")
+    return usernNamefromInput
 
-print("\nTo begin, please enter your username")
 
-#get input from user into terminal
 
-username = input()
-print("Hello, " + username)
+# greet user
+def greetUser(name):
+    print("Hello, " + name)
+
+welcomeuser()
+userName = getUsername()
+greetUser(userName)
