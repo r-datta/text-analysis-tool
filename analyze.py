c431cf17422fc2871dd0e339592a43f03a3f050c
Rahul Datta <rahuldatta12@hotmail.com>
2025-02-25 14:26:54 -0500

    added while loops for username entry

--- analyze.py
+++ analyze.py
@@ -1,23 +1,40 @@
 from random_username.generate import generate_username
 
-
 # welcome user
 def welcomeuser():
     print("\nWelcome to the text analysis tool, I will mine and analyze a body of text from a file you provide")
 
 # Get user name 
 def getUsername():
-    userNamefromInput = input("\nTo begin, please enter your username:\n")
+
+    maxAttempts = 3
+    attempts = 0
+
+    while attempts < maxAttempts:
+        # print message prompting user to input their name
+        inputPrompt = ""
+        if attempts == 0:
+            inputPrompt = "\nTo begin, please enter your username:\n"
+        else: 
+            inputPrompt = "\nPlease try again:\n"
+        userNamefromInput = input(inputPrompt)
+
+#validate username
+        if len(userNamefromInput) < 5 or not userNamefromInput.isidentifier():
+             print("Your username must be at least 5 characters long, alphanumeric only (a-z/A-Z/0-9), have no spaces and cannot start with a number")
+        else: 
+            return userNamefromInput
+        attempts += 1
+    
+    print("\nExhausted all " + str(maxAttempts) + " attempts, assigning new username: ")
+    return generate_username()[0]
     
     
 
     
-    if len(userNamefromInput) < 5 or not userNamefromInput.isidentifier():
-        print("Your username must be at least 5 characters long, alphanumeric only (a-z/A-Z/0-9), have no spaces and cannot start with a number")
-        userNamefromInput = generate_username()[0]
-        print("Assigning new username: " + userNamefromInput)
-
-    return userNamefromInput
+    
+        
+        
 
 # greet user
 def greetUser(name):
